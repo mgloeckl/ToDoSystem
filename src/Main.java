fe20f5cae3e5c9d5a35882109831be98bfc8ac3a
jo5jo <R8Mroq4>
2020-11-27 08:45:32 +0100

    again

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import DatabaseHandler.DatabaseHandler;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Main extends Application implements Initializable {
 
     /**
      * @ToDo edit run configuration
@@ -29,5 +34,11 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        DatabaseHandler databaseHandler = DatabaseHandler.getInstance();
+        databaseHandler.setConn(DatabaseHandler.generateConnector());
     }
 }
