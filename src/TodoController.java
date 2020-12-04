6fc790dd97eb47d6c7167012ff65253ef8494184
Marco Gloeckl <marcogloeckl37@gmail.com>
2020-12-04 20:52:34 +0100

    Daten von displayItem werden angezeigt

--- src/TodoController.java
+++ src/TodoController.java
@@ -2,6 +2,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import model.Priority;
 import model.Status;
 import model.ToDo;
 
@@ -9,13 +10,18 @@ public class TodoController {
     public TextField nameTextField;
     public TextArea descriptionTextArea;
     public ComboBox<Status> statusComboBox;
-    public ComboBox priorityComboBox;
+    public ComboBox<Priority> priorityComboBox;
 
     private ToDo selected = null;
 
     public void setToDo(ToDo item){
         selected = item;
         displayItem();
+    }
+
+    public void initialize(){
+        priorityComboBox.setItems(Priority.getList());
+        statusComboBox.setItems(Status.getList());
     }
 
     private void displayItem(){
@@ -25,15 +31,17 @@ public class TodoController {
         //statusComboBox.setItems();
 
         nameTextField.setText(selected.getName());
+        descriptionTextArea.setText(selected.getDescription());
 
         int i = 0;
         for (i = 0; i < statusComboBox.getItems().size(); i++){
             if(statusComboBox.getItems().get(i).getId() == selected.getStatus_id()){
-                break;
+                statusComboBox.getSelectionModel().select(i);
+            }
+            if(statusComboBox.getItems().get(i).getId() == selected.getPriority_id()){
+                priorityComboBox.getSelectionModel().select(i);
             }
         }
-
-        statusComboBox.getSelectionModel().select(i);
     }
 
     public void cancelClicked(ActionEvent actionEvent) {
