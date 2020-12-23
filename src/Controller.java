3592456f547bd9aece4303c7ffe7a2fff01a8831
jo5jo <R8Mroq4>
2020-12-23 09:34:27 +0100

    filtering

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,6 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,6 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -15,6 +19,7 @@ import model.Status;
 import model.ToDo;
 
 import java.io.IOException;
+import java.util.function.Predicate;
 
 public class Controller {
 
@@ -26,9 +31,11 @@ public class Controller {
 
     ToDo selectedItem = null;
     private ObservableList<ToDo> todoList;
-    private ObservableList<Status> statusList;
-    private ObservableList<Priority> priorityList;
-
+    private ObservableList<Status> statusList = FXCollections.observableArrayList();
+    private ObservableList<Priority> priorityList = FXCollections.observableArrayList();
+    private Status selectedStatus = new Status("", -69);
+    private Priority selectedPriority = new Priority(-69, "");
+    private String selectedName = "";
     public void setToDoList(ObservableList<ToDo> list){
         this.todoList = list;
     }
@@ -62,7 +69,10 @@ public class Controller {
     }
 
     public void initialize(){
-        ToDoListView.setItems(ToDo.getTodo());
+        selectedStatus = new Status("", -69);
+        selectedPriority = new Priority(-69, "");
+        todoList = ToDo.getTodo();
+        ToDoListView.setItems(todoList);
         priorityComboBox.setItems(Priority.getList());
         statusComboBox.setItems(Status.getList());
         loadComboBox();
@@ -107,23 +117,73 @@ public class Controller {
     }
     private void loadComboBox() {
         statusList.add(new Status("All", -69));
+        statusComboBox.getSelectionModel().select(new Status("All", -69));
         statusList = FXCollections.concat(statusList, Status.getList());
         statusComboBox.setItems(statusList);
 
         priorityList.add(new Priority(-69, "All"));
+        priorityComboBox.getSelectionModel().select(new Priority(-69, "All"));
         priorityList = FXCollections.concat(priorityList, Priority.getList());
         priorityComboBox.setItems(priorityList);
     }
 
     private void filter(){
+        /*todoList.filtered(new Predicate<ToDo>() {
+            @Override
+            public boolean test(ToDo toDo) {
+                //if(selectedStatus.getId()==-69 && selectedPriority.getId() ==-69){
+                  //  return true;
+                //}
+                if(toDo.getStatus_id()==selectedStatus.getId() && selectedPriority.getId() ==-69){
+                    return true;
+                }
+                return true;
+            }
+        });
+        ToDoListView.setItems(todoList);*/
 
+        FilteredList<ToDo> filterdata = new FilteredList<>(todoList, predicate -> true);
+        //filterdata.addListener((Change change));
+        filterdata.setPredicate(ToDo -> {
+        if(selectedName.equals("")) {
+            if (selectedStatus.getId() == -69 && selectedPriority.getId() == -69) {
+                return true;
+            } else if (selectedStatus.getId() == ToDo.getStatus_id() && selectedPriority.getId() == -69) {
+                return true;
+            } else if (selectedStatus.getId() == -69 && selectedPriority.getId() == ToDo.getPriority_id()) {
+                return true;
+            } else if (selectedStatus.getId() == ToDo.getStatus_id() && selectedPriority.getId() == ToDo.getPriority_id()) {
+                return true;//koks
+            }
+        }else if (!selectedName.equals("")){
+            if (selectedStatus.getId() == -69 && selectedPriority.getId() == -69 && ToDo.getName().trim().replaceAll(" ", "").toLowerCase().contains(selectedName.toLowerCase().trim().replaceAll(" ", ""))) {
+                return true;
+            } else if (selectedStatus.getId() == ToDo.getStatus_id() && selectedPriority.getId() == -69 && ToDo.getName().trim().replaceAll(" ", "").toLowerCase().contains(selectedName.toLowerCase().trim().replaceAll(" ", ""))) {
+                return true;
+            } else if (selectedStatus.getId() == -69 && selectedPriority.getId() == ToDo.getPriority_id() && ToDo.getName().trim().replaceAll(" ", "").toLowerCase().contains(selectedName.toLowerCase().trim().replaceAll(" ", ""))) {
+                return true;
+            } else if (selectedStatus.getId() == ToDo.getStatus_id() && selectedPriority.getId() == ToDo.getPriority_id() && ToDo.getName().trim().replaceAll(" ", "").toLowerCase().contains(selectedName.toLowerCase().trim().replaceAll(" ", ""))) {
+                return true;//koks
+            }
+        }
+
+            return false;
+        });
+        ToDoListView.setItems(filterdata);
     }
 
     public void onStatusComboBoxClicked(ActionEvent actionEvent) {
-
+        selectedStatus = statusComboBox.getValue();
+        filter();
     }
 
     public void onPriorityComboBoxClicked(ActionEvent actionEvent) {
+        selectedPriority = priorityComboBox.getValue();
+        filter();
+    }
 
+    public void search_name(KeyEvent keyEvent) {
+            selectedName = ToDoNameTextField.getText().toString();
+            filter();
     }
 }
